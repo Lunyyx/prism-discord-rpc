c1d7eeeb1479183377780b0847fcf8fa0e39b242
Lunyyx <thedarkmaster71@hotmail.com>
2026-08-16 17:50:46 +0200

    Configure default log level as info

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,10 @@ struct DiscordClient {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
+    env_logger::Builder::from_env(
+        env_logger::Env::default().default_filter_or("info")
+    )
+    .init();
 
     monitor()
 }
